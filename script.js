77ce79553aa1a22c79c4454cc2de3a64c659cf73
JayGMZ <65398040+JayGMZ@users.noreply.github.com>
2021-11-21 17:01:08 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const quoteApiUrl = "https://api.quotable.io/random?minLength=100&maxLength=100";
+const quoteApiUrl = "https://api.quotable.io/random?minLength=80&maxLength=100";
 const quoteSection = document.getElementById("quote");
 const userInput = document.getElementById("quote-input");
 let quote = "";
